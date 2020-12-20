bda2a07e8eaea3475e1b09ad028d3b998634b63d
thanhlam1702 <thanhlam17021999@gmail.com>
2020-12-20 20:25:47 +0700

    mess chat pages

--- pages/message/_id.vue
+++ pages/message/_id.vue
@@ -1,3 +1,70 @@
 <template>
-  <div></div>
+  <main class="main-wrapper message">
+    <div class="message__left">
+      <ul class="chats">
+        <li v-for="(item, index) in dataChats" :key="index">
+          <nuxt-link class="chats__item" :to="'/message/' + item.id">
+            <div class="chats__item-img">
+              <img src="~/assets/images/avatart-default.jpg" alt="" />
+            </div>
+            <div class="chats__item-name">{{ item.name }}</div>
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <div class="message__right">
+      <div class="message__right-top">
+        <div class="img">
+          <img src="~/assets/images/avatart-default.jpg" alt="" />
+        </div>
+        <div>
+          {{ name }}
+        </div>
+      </div>
+      <div class="message__right-content">
+        <!-- content chat -->
+      </div>
+      <div class="message__right-control">
+        <form class="form-mess">
+          <textarea
+            name="mess-box"
+            class="form-mess__box"
+            placeholder="Ae"
+          ></textarea>
+        </form>
+        <div class="icon-send">
+          <i class="icon">
+            <img src="~/assets/images/icon/send-mess.svg" alt="" />
+          </i>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      dataChats: [
+        { name: 'Tuan Huynh', id: '123' },
+        { name: 'Van A', id: '124' },
+        { name: 'Van B', id: '125' },
+        { name: 'Van C', id: '126' },
+        { name: 'Van D', id: '127' },
+        { name: 'Cao Tuan Tai', id: '128' },
+        { name: 'Tran Thi Mong Mo', id: '103' },
+        { name: 'Ly Dai Phuc', id: '139' },
+        { name: 'Ly Dai Phuc', id: '149' },
+        { name: 'Ly Dai Phuc', id: '169' },
+      ],
+    }
+  },
+  mounted() {
+    this.dataChats.filter((item) =>
+      item.id === this.$route.params.id ? (this.name = item.name) : null
+    )
+    console.log(this.name)
+  },
+}
+</script>
--- pages/message/index.vue
+++ pages/message/index.vue
@@ -2,26 +2,36 @@
   <main class="main-wrapper message">
     <div class="message__left">
       <ul class="chats">
-        <li class="chat__item">
-          <div class="chat__item-img">
-            <img src="~/assets/images/avatart-default.jpg" alt="" />
-          </div>
-          <div class="chat__item-name">Tuan Huynh</div>
-        </li>
-        <li class="chat__item">
-          <div class="chat__item-img">
-            <img src="~/assets/images/avatart-default.jpg" alt="" />
-          </div>
-          <div class="chat__item-name">Tuan Huynh</div>
-        </li>
-        <li class="chat__item">
-          <div class="chat__item-img">
-            <img src="~/assets/images/avatart-default.jpg" alt="" />
-          </div>
-          <div class="chat__item-name">Tuan Huynh</div>
+        <li v-for="(item, index) in dataChats" :key="index">
+          <nuxt-link class="chats__item" :to="'/message/' + item.id">
+            <div class="chats__item-img">
+              <img src="~/assets/images/avatart-default.jpg" alt="" />
+            </div>
+            <div class="chats__item-name">{{ item.name }}</div>
+          </nuxt-link>
         </li>
       </ul>
     </div>
     <div class="messaga__right"></div>
   </main>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      dataChats: [
+        { name: 'Tuan Huynh', id: '123' },
+        { name: 'Van A', id: '124' },
+        { name: 'Van B', id: '125' },
+        { name: 'Van C', id: '126' },
+        { name: 'Van D', id: '127' },
+        { name: 'Cao Tuan Tai', id: '128' },
+        { name: 'Tran Thi Mong Mo', id: '103' },
+        { name: 'Ly Dai Phuc', id: '139' },
+        { name: 'Ly Dai Phuc', id: '149' },
+        { name: 'Ly Dai Phuc', id: '169' },
+      ],
+    }
+  },
+}
+</script>
